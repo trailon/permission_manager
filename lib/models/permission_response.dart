b1569e0a874abb6aa126826d1f60cfea24b80562
trailon <cetinelv@gmail.com>
2025-05-25 16:52:29 +0300

    update permission manager dependencies & clear wildcard warning

--- lib/models/permission_response.dart
+++ lib/models/permission_response.dart
@@ -7,12 +7,12 @@ class PermissionResponse {
       {this.infoplistkeys, this.androidmanifestperms}) {
     debugPrint(toMap().toString());
   }
-  bool get isAllGranted => permissionStatus.every((_) => _.isGranted);
-  bool get isAnyDenied => permissionStatus.any((_) => _.isDenied);
-  Map<String,String>? infoplistkeys = {};
-  Map<String,String>? androidmanifestperms = {};
+  bool get isAllGranted => permissionStatus.every((status) => status.isGranted);
+  bool get isAnyDenied => permissionStatus.any((status) => status.isDenied);
+  Map<String, String>? infoplistkeys = {};
+  Map<String, String>? androidmanifestperms = {};
 
-  Map<String, Map<String,String>> toMap() {
+  Map<String, Map<String, String>> toMap() {
     return {
       "infoplists": infoplistkeys ?? {},
       "androidmanifests": androidmanifestperms ?? {},
